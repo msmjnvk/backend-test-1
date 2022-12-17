5407d07de85065cfc995df4e1873749a561ea389
msmjnvk <msmjnvk@gmail.com>
2022-12-17 13:59:32 +0400

    generate token api

--- Part_1/index.js
+++ Part_1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const dotenv = require('dotenv');
 const { validationResult, check } = require('express-validator');
 const fs = require('fs');
 var slugify = require('slugify');
 const multer = require("multer");
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const upload = multer({
@@ -17,12 +19,41 @@ const upload = multer({
     })
 });
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, imagePath) => {
+      console.log(err)
+      if (err) return res.sendStatus(403)
+      req.body.imagePath = imagePath
+      next()
+    })
+  }
+
+dotenv.config();
+process.env.TOKEN_SECRET;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 'use strict';
 
 Date.prototype.toUnixTime = function() { return this.getTime()/1000|0 };
     Date.time = function() { return new Date().toUnixTime(); }
+
+function generateAccessToken(imagePath) {
+        return jwt.sign(imagePath, process.env.TOKEN_SECRET, { expiresIn: '300s' });
+      }
+
+app.post('/api/newtoken', (req, res) => {
+    const token = generateAccessToken({ imagePath: req.body.imagePath});
+    res.json({accessToken: token});
+});
+
+app.get('/api/getimage', authenticateToken, (req, res) => {
+    res.send(true);
+  });
 
 app.post('/api/post/add', fileUpload({ createParentPath: true }),
     check('title')
@@ -48,13 +79,13 @@ app.post('/api/post/add', fileUpload({ createParentPath: true }),
             return res.status(400).json({ errors: 'Main Image required' });
         }
         if (files.main_image.size > 1000000) {
-            return res.status(400).json({ errors: 'Image size should be less than 1Mb' });
+            return res.status(400).json({ errors: 'Main Image size should be less than 1Mb' });
         }
         const file_extension = files.main_image.name.slice(
             ((files.main_image.name.lastIndexOf('.') - 1) >>> 0) + 2
         );
         if (!array_of_allowed_files.includes(file_extension)) {
-            return res.status(400).json({ errors: 'Image foramt should be jpg' });
+            return res.status(400).json({ errors: 'Main Image foramt should be jpg' });
         }
         if (files.additional_images && files.additional_images.length > 5) {
             return res.status(400).json({ errors: 'Number of additional image Max 5' });
